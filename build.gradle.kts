96a40fdac34a0fe60399a3a9386fa95734b1c735
Felstr07 <felix.stramke@gmail.com>
2026-01-16 22:32:33 +0100

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-// In app/build.gradle.kts
-
-android {
-    // ... other configurations like compileSdk, defaultConfig ...
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17" // Or "1.8" if you use an older setup
-    }
-
-    // ... other configurations like buildFeatures, composeOptions ...
-}
-
 
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
 }
